3c6d76fdc938f9b6ea4a95abc93211be9a1ab967
Ar Iskandar <arrrriskandar@gmail.com>
2024-05-19 14:40:55 +0800

    Fixed password validation to include a wider range of special character

--- client/src/components/Auth/RegisterForm.js
+++ client/src/components/Auth/RegisterForm.js
@@ -18,7 +18,7 @@ const RegisterForm = ({ toggleRegister }) => {
   const validatePassword = (_, value) => {
     // Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character
     const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?`~])[A-Za-z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?`~]{8,}$/;
     if (!value || value.match(passwordRegex)) {
       return Promise.resolve();
     }
