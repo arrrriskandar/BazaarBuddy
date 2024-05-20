fd151be558b15d95ffd27051e59228fccf11a463
Ar Iskandar <arrrriskandar@gmail.com>
2024-05-20 15:04:22 +0800

    Improve the UI for login and register

--- client/src/components/Auth/LoginForm.js
+++ client/src/components/Auth/LoginForm.js
@@ -1,12 +1,13 @@
-import { Form, Input, Button, message, Typography } from "antd";
+import React, { useState } from "react";
+import { Form, Input, Button, message, Typography, Card, Row, Col } from "antd";
 import { login } from "../../firebase/auth";
-import { useState } from "react";
 import ResetPasswordModal from "./ResetPasswordModal";
 
 const LoginForm = ({ toggleRegister }) => {
   const [form] = Form.useForm();
   const [resetPasswordModal, setResetPasswordModal] = useState(false);
   const { Text } = Typography;
+
   const handleForgotPasswordClick = () => {
     setResetPasswordModal(true);
   };
@@ -25,62 +26,81 @@ const LoginForm = ({ toggleRegister }) => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <Form form={form} onFinish={onFinish}>
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: "email",
-              message: "Please enter a valid email address!",
-            },
-          ]}
-        >
-          <Input placeholder="Email" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please enter your password!",
-            },
-          ]}
-        >
-          <Input.Password placeholder="Password" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
-      <Text
-        style={{
-          cursor: "pointer",
-          color: "#1890ff",
-        }}
-        onClick={toggleRegister}
-      >
-        Don't have an account? Register here.
-      </Text>
-      <br />
-      <Text
-        style={{
-          cursor: "pointer",
-          color: "#1890ff",
-        }}
-        onClick={handleForgotPasswordClick}
-      >
-        Forgot password?
-      </Text>
-      <ResetPasswordModal
-        visible={resetPasswordModal}
-        onClose={handleForgotPasswordClose}
-      />
-    </div>
+    <Row
+      justify="center"
+      align="middle"
+      style={{ minHeight: "100vh", backgroundColor: "#f0f2f5" }}
+    >
+      <Col>
+        <Card bordered={true} style={{ width: 400, textAlign: "center" }}>
+          <img
+            src="/logo1.png"
+            alt="BazaarBuddy"
+            style={{
+              marginBottom: 20,
+              maxWidth: "100%",
+              height: "auto",
+              maxHeight: 200,
+              borderRadius: 100,
+            }}
+          />
+          <Typography.Title level={2}>Login</Typography.Title>
+          <Form form={form} onFinish={onFinish}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  type: "email",
+                  message: "Please enter a valid email address!",
+                },
+              ]}
+            >
+              <Input placeholder="Email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your password!",
+                },
+              ]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+          <Text
+            style={{
+              cursor: "pointer",
+              color: "#1890ff",
+            }}
+            onClick={toggleRegister}
+          >
+            Don't have an account? Register here.
+          </Text>
+          <br />
+          <Text
+            style={{
+              cursor: "pointer",
+              color: "#1890ff",
+            }}
+            onClick={handleForgotPasswordClick}
+          >
+            Forgot password?
+          </Text>
+          <ResetPasswordModal
+            visible={resetPasswordModal}
+            onClose={handleForgotPasswordClose}
+          />
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
--- client/src/components/Auth/RegisterForm.js
+++ client/src/components/Auth/RegisterForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Input, Button, message, Typography } from "antd";
+import { Form, Input, Button, message, Typography, Card, Row, Col } from "antd";
 import { register } from "../../firebase/auth";
 
 const RegisterForm = ({ toggleRegister }) => {
@@ -16,7 +16,6 @@ const RegisterForm = ({ toggleRegister }) => {
   };
 
   const validatePassword = (_, value) => {
-    // Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character
     const passwordRegex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?`~])[A-Za-z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?`~]{8,}$/;
     if (!value || value.match(passwordRegex)) {
@@ -28,51 +27,70 @@ const RegisterForm = ({ toggleRegister }) => {
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      <Form form={form} onFinish={onFinish}>
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: "email",
-              message: "Please enter a valid email address!",
-            },
-          ]}
-        >
-          <Input placeholder="Email" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please enter a password!",
-            },
-            {
-              validator: validatePassword,
-            },
-          ]}
-        >
-          <Input.Password placeholder="Password" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
-      <Text
-        style={{
-          cursor: "pointer",
-          color: "#1890ff",
-        }}
-        onClick={toggleRegister}
-      >
-        Already have an account? Login here.
-      </Text>
-    </div>
+    <Row
+      justify="center"
+      align="middle"
+      style={{ minHeight: "100vh", backgroundColor: "#f0f2f5" }}
+    >
+      <Col>
+        <Card bordered={true} style={{ width: 400, textAlign: "center" }}>
+          <img
+            src="/logo1.png"
+            alt="BazaarBuddy"
+            style={{
+              marginBottom: 20,
+              maxWidth: "100%",
+              height: "auto",
+              maxHeight: 200,
+              borderRadius: 100,
+            }}
+          />
+          <Typography.Title level={2}>Register</Typography.Title>
+          <Form form={form} onFinish={onFinish}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  type: "email",
+                  message: "Please enter a valid email address!",
+                },
+              ]}
+            >
+              <Input placeholder="Email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter a password!",
+                },
+                {
+                  validator: validatePassword,
+                },
+              ]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+          <Text
+            style={{
+              cursor: "pointer",
+              color: "#1890ff",
+            }}
+            onClick={toggleRegister}
+          >
+            Already have an account? Login here.
+          </Text>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
