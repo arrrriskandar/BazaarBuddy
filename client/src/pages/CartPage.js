50dcb2b1c82243136423895ec440cb7ce9b6d40b
kerinwlt <kerinwong97@gmail.com>
2024-06-12 16:49:58 +0800

    your cart is empty client

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -10,6 +10,8 @@ import {
   Space,
   message,
 } from "antd";
+import { ShoppingOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const { carts, removeFromCart, updateQuantity, deleteCart } = useCart();
@@ -49,86 +51,112 @@ const Cart = () => {
 
   return (
     <div>
-      {carts.map((cart) => (
-        <div key={cart._id} style={{ marginBottom: "20px" }}>
-          <Row justify="space-between" align="middle">
-            <Col>
-              <Row align="middle">
-                <Avatar
-                  src={cart.seller.photoUrl || "/default-avatar.png"}
-                  style={{ marginRight: "10px" }}
-                />
-                <h1 style={{ margin: 0 }}>{cart.seller.username}</h1>
-              </Row>
-            </Col>
-            <Col>
-              <Button danger type="text" onClick={() => handleDelete(cart._id)}>
-                x
-              </Button>
-            </Col>
-          </Row>
-          <List
-            itemLayout="horizontal"
-            dataSource={cart.items}
-            renderItem={(item) => (
-              <List.Item>
-                <Row style={{ width: "100%" }} align="top" gutter={16}>
-                  <Col>
-                    <Avatar
-                      shape="square"
-                      src={item.product.images}
-                      size={200}
-                    />
-                  </Col>
-                  <Col flex="auto">
-                    <div
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        height: "100%",
-                      }}
-                    >
-                      <h2 style={{ margin: 0 }}>{item.product.name}</h2>
-                      <Space direction="vertical" style={{ marginTop: "10px" }}>
-                        <div>Price: ${item.product.price}</div>
-                        <Row align="middle">
-                          <Col>
-                            <span>Quantity: </span>
-                          </Col>
-                          <Col>
-                            <InputNumber
-                              min={1}
-                              value={item.quantity}
-                              onChange={(value) =>
-                                handleQuantityChange(
-                                  cart._id,
-                                  item.product._id,
-                                  value
-                                )
-                              }
-                            />
-                          </Col>
-                        </Row>
-                      </Space>
-                    </div>
-                  </Col>
-                  <Col>
-                    {cart.items.length > 1 && (
-                      <Button
-                        danger
-                        onClick={() => handleRemove(cart._id, item.product._id)}
-                      >
-                        Remove
-                      </Button>
-                    )}
-                  </Col>
+      {carts.length > 0 ? (
+        carts.map((cart) => (
+          <div key={cart._id} style={{ marginBottom: "20px" }}>
+            <Row justify="space-between" align="middle">
+              <Col>
+                <Row align="middle">
+                  <Avatar
+                    src={cart.seller.photoUrl || "/default-avatar.png"}
+                    style={{ marginRight: "10px" }}
+                  />
+                  <h1 style={{ margin: 0 }}>{cart.seller.username}</h1>
                 </Row>
-              </List.Item>
-            )}
-          />
+              </Col>
+              <Col>
+                <Button
+                  danger
+                  type="text"
+                  onClick={() => handleDelete(cart._id)}
+                >
+                  x
+                </Button>
+              </Col>
+            </Row>
+            <List
+              itemLayout="horizontal"
+              dataSource={cart.items}
+              renderItem={(item) => (
+                <List.Item>
+                  <Row style={{ width: "100%" }} align="top" gutter={16}>
+                    <Col>
+                      <Avatar
+                        shape="square"
+                        src={item.product.images}
+                        size={200}
+                      />
+                    </Col>
+                    <Col flex="auto">
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          height: "100%",
+                        }}
+                      >
+                        <h2 style={{ margin: 0 }}>{item.product.name}</h2>
+                        <Space
+                          direction="vertical"
+                          style={{ marginTop: "10px" }}
+                        >
+                          <div>Price: ${item.product.price}</div>
+                          <Row align="middle">
+                            <Col>
+                              <span>Quantity: </span>
+                            </Col>
+                            <Col>
+                              <InputNumber
+                                min={1}
+                                value={item.quantity}
+                                onChange={(value) =>
+                                  handleQuantityChange(
+                                    cart._id,
+                                    item.product._id,
+                                    value
+                                  )
+                                }
+                              />
+                            </Col>
+                          </Row>
+                        </Space>
+                      </div>
+                    </Col>
+                    <Col>
+                      {cart.items.length > 1 && (
+                        <Button
+                          danger
+                          onClick={() =>
+                            handleRemove(cart._id, item.product._id)
+                          }
+                        >
+                          Remove
+                        </Button>
+                      )}
+                    </Col>
+                  </Row>
+                </List.Item>
+              )}
+            />
+          </div>
+        ))
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            minHeight: "50vh",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <h2>Your cart is empty</h2>
+          <Button type="primary" icon={<ShoppingOutlined />}>
+            <Link to="/buy/product">Continue Shopping</Link>
+          </Button>
         </div>
-      ))}
+      )}
     </div>
   );
 };
