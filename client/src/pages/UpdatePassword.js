3198c068c3329a4878e0d27eaca404885691137a
Ar Iskandar <arrrriskandar@gmail.com>
2026-04-16 17:00:33 +0800

    Create UpdatePassword.js
    Create util to validate password

---
+++ client/src/pages/UpdatePassword.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Form,
+  Input,
+  Button,
+  message,
+  Typography,
+  Card,
+  Row,
+  Col,
+  Avatar,
+} from "antd";
+import { validatePassword } from "../utils/validatePassword";
+import { handleUpdatePassword } from "../supabase/auth";
+
+const UpdatePassword = () => {
+  const onFinish = async (values) => {
+    try {
+      await handleUpdatePassword(values.password);
+      message.success("Registration successful!");
+    } catch (error) {
+      message.error("Registration failed");
+      console.error("Registration failed", error);
+    }
+  };
+
+  return (
+    <Row
+      justify="center"
+      align="middle"
+      style={{ minHeight: "100vh", backgroundColor: "#f0f2f5" }}
+    >
+      <Col>
+        <Card bordered={true} style={{ width: 400, textAlign: "center" }}>
+          <Avatar src="/logo1.png" alt="BazaarBuddy" size={200} />
+          <Typography.Title level={2}>Update Password</Typography.Title>
+          <Form name="updatePasswordForm" onFinish={onFinish}>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter a password!",
+                },
+                {
+                  validator: validatePassword,
+                },
+              ]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Update Password
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default UpdatePassword;
