646a46c33d822cb8d58004954afab077237f42ba
Ar Iskandar <arrrriskandar@gmail.com>
2026-04-22 14:47:13 +0800

    Updated package and env

--- client/src/supabase/config.js
+++ client/src/supabase/config.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://lofycmbjcsqrryuusgoy.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvZnljbWJqY3NxcnJ5dXVzZ295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNDgxMTUsImV4cCI6MjA5MTcyNDExNX0.16CwBDSLpwYpyyIiTuQ_0WRooo-2pPjsZ7w_hy7xXYk";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
